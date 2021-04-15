1d9a730eb2b1247b4c7ec9c57e40288e9352446e
Jay Anton Roblico <jayantonroblico23@gmail.com>
2021-04-15 13:00:22 +0800

    :lipstick: Mobile Response

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let pop = document.querySelector(".pop-up");
 share.addEventListener("click", () => {
   if (pop.style.display === "none") {
     pop.style.display = "flex";
+    pop.style.cursor = "default";
   } else {
     pop.style.display = "none";
   }
